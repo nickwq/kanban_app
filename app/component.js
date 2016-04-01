ed7fffd51cb6ce62ed01041352f6e47e1886af1b
Wang Qian <nickwq@gmail.com>
2016-04-01 23:33:49 +0800

    set up dev webpack config

--- app/component.js
+++ app/component.js
@@ -1,6 +1,7 @@
 module.exports = function () {
     var element = document.createElement('h1');
 
-    element.innerHTML = "Hello World";
+    element.innerHTML = "Hello test World";
+
     return element;
 };
