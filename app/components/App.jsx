f284249f421cbde4d3f9da6c527f4ed68733b020
Wang Qian <nickwq@gmail.com>
2016-04-04 13:56:20 +0800

    support remove note

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -30,7 +30,11 @@ class App extends React.Component {
         const notes = this.state.notes;
         return (<div>
             <button onClick={this.addNote}>+</button>
-            <Notes notes={notes}/>
+            <Notes
+                notes={notes}
+                onEdit={this.editNote}
+                onDelete={this.deleteNote}
+            />
         </div>);
     }
 
@@ -40,6 +44,29 @@ class App extends React.Component {
                 id: uuid.v4(),
                 task: 'New task'
             }])
+        });
+    };
+
+    editNote = (id, task) => {
+        if (!task.trim()) {
+            return;
+        }
+
+        const notes = this.state.notes.map(note => {
+            if (note.id === id && task) {
+                note.task = task;
+            }
+            return note;
+        });
+
+        this.setState({notes});
+    };
+
+    deleteNote = (id, e) => {
+        e.stopPropagation();
+
+        this.setState({
+            notes: this.state.notes.filter(note => note.id !== id)
         });
     };
 }
