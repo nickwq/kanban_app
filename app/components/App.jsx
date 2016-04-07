cf23bba8e2d70c3e66a6dc0e724316a5ab188d87
Wang Qian <nickwq@gmail.com>
2016-04-07 22:36:34 +0800

    use alt container

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,34 +2,39 @@ import React from 'react';
 import Notes from './Notes.jsx';
 import NoteActions  from '../actions/NoteActions.js';
 import NoteStore from '../stores/NoteStore.js';
+import AltContainer from 'alt-container';
+
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
+    //
+    //     this.state = NoteStore.getState();
+    //
+    // }
 
-        this.state = NoteStore.getState();
-
-    }
-
-    componentDidMount(){
-        NoteStore.listen(this.storeChanged);
-    }
-    componentWillUnmount(){
-        NoteStore.unlisten(this.storeChanged);
-    }
-    storeChanged = (state) => {
-        this.setState(state);
-    };
+    // componentDidMount(){
+    //     NoteStore.listen(this.storeChanged);
+    // }
+    // componentWillUnmount(){
+    //     NoteStore.unlisten(this.storeChanged);
+    // }
+    // storeChanged = (state) => {
+    //     this.setState(state);
+    // };
 
     render() {
-        const notes = this.state.notes;
+        // const notes = this.state.notes;
         return (<div>
             <button className="add-note" onClick={this.addNote}>+</button>
-            <Notes
-                notes={notes}
-                onEdit={this.editNote}
-                onDelete={this.deleteNote}
-            />
+            <AltContainer
+                stores={[NoteStore]}
+                inject={{
+                    notes: () => NoteStore.getState().notes
+                }}
+            >
+                <Notes onEdit={this.editNote} onDelete={this.deleteNote}/>
+            </AltContainer>
         </div>);
     }
 
