221d250c16336eb3231d3d5915a5b3383b46edbb
Wang Qian <nickwq@gmail.com>
2016-04-10 22:45:05 +0800

    prepare the packages for drag and rop

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,33 +1,15 @@
 import React from 'react';
-// import Notes from './Notes.jsx';
-// import NoteActions  from '../actions/NoteActions.js';
-// import NoteStore from '../stores/NoteStore.js';
 import AltContainer from 'alt-container';
 import Lanes from './Lanes';
 import LaneActions from '../actions/LaneActions';
 import LaneStore from '../stores/LaneStore';
+import {DragDropContext} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
-
+@DragDropContext(HTML5Backend)
 export default class App extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //
-    //     this.state = NoteStore.getState();
-    //
-    // }
-
-    // componentDidMount(){
-    //     NoteStore.listen(this.storeChanged);
-    // }
-    // componentWillUnmount(){
-    //     NoteStore.unlisten(this.storeChanged);
-    // }
-    // storeChanged = (state) => {
-    //     this.setState(state);
-    // };
 
     render() {
-        // const notes = this.state.notes;
         return (<div>
             <button className="add-lane" onClick={this.addLane}>+</button>
             <AltContainer
