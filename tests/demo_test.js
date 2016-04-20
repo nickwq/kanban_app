1ac0eb6dc0172b15a955639bf9030062743543ea
Wang Qian <nickwq@gmail.com>
2016-04-21 00:31:06 +0800

    test Editabel render value and click action

--- tests/demo_test.js
+++ tests/demo_test.js
@@ -2,7 +2,7 @@ import assert from 'assert';
 
 describe('add', () => {
     it('adds', () => {
-        assert.equal(1+1, 2);
+        assert.equal(1+2, 3);
     });
 });
 
---
+++ tests/editable_test.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import  {
+    renderIntoDocument,
+    findRenderedDOMComponentWithClass,
+    findRenderedDOMComponentWithTag,
+    Simulate
+} from 'react-addons-test-utils';
+import assert from 'assert';
+import Editable from '../app/components/Editable.jsx'
+
+describe('Editable', () => {
+    it('renders value', () => {
+        const value = 'value';
+        const component = renderIntoDocument(
+            <Editable value={value}/>
+        );
+
+        const valueComponent = findRenderedDOMComponentWithClass(component, 'value');
+        assert.equal(valueComponent.textContent, value)
+    });
+
+    it('triggers onValueClick', () => {
+        let triggered = false;
+        const value = 'value';
+        const onValueClick = () =>  { triggered = true;};
+        const component = renderIntoDocument(
+            <Editable value={value} onValueClick={onValueClick}/>
+        );
+
+        const valueComponent = findRenderedDOMComponentWithClass(component, 'value');
+        Simulate.click(valueComponent);
+
+        assert.equal(triggered, true)
+
+    });
+});
